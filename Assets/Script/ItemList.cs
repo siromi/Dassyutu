b97d6eec1b1669a2535a76536d370f21c4ebd999
name <you@gmail.com>
2019-07-03 20:43:03 +0900

    init List

---
+++ Assets/Script/ItemList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemList : MonoBehaviour
+{
+    static ItemList itemList = new ItemList();
+
+    string imageFolderName = "Image/"; //アイテムの画像が入っているフォルダ名
+    string noItemImage = "no_item"; //何も入ってないときに表示する画像
+
+  public  List<GameObject> itemButtonList=new List<GameObject>(); //アイテムリスト（ボタン）の番号
+
+
+    //何故かnew出来ないので、このメソッドでスプリクトを返す
+    public static ItemList GetInstance()
+    {
+        return itemList;
+    }
+
+    //リストにボタンを入れる、画像を初期化
+    public void  InitItemList()
+    {
+        int i=0; //ボタンの番号
+        while (GameObject.Find("Button ("+i.ToString() + ")")!=null) 
+        {
+            itemButtonList.Add(GameObject.Find("Button (" + i.ToString() + ")"));
+
+            itemButtonList[i].GetComponent<Image>().sprite = Resources.Load(imageFolderName + noItemImage, typeof(Sprite)) as Sprite;
+            i++;
+        }
+    }
+
+
+}
---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+    ItemList itemList; //スプリクトを入れる
+
+
+	// Use this for initialization
+	void Start () {
+        itemList = ItemList.GetInstance();
+        itemList.InitItemList();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
